ccae4f7d058db6c8597e160d8bcb5d10ff1a307d
Allan Moo <tastenumber5@gmail.com>
2023-05-16 10:54:41 +0930

    first commit
    section hero -completed
    section about -completed

---
+++ public/src/main.js
@@ -0,0 +1,6 @@
+import "../styles/modern-normalize.css";
+import "../styles/style.css";
+import "../components/header.css";
+import "../styles/utils.css";
+import "../styles/about.css";
+import "../styles/hero.css";
