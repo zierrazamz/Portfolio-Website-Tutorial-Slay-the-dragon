0a8cc9f0cfb61571465b7ebf0cb6861bad113beb
Allan Moo <tastenumber5@gmail.com>
2023-05-17 13:38:39 +0930

    adding images - working with grid

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import "../styles/modern-normalize.css";
+import "../styles/style.css";
+import "../components/header.css";
+import "../styles/utils.css";
+import "../styles/about.css";
+import "../styles/featured.css";
+import "../styles/hero.css";
